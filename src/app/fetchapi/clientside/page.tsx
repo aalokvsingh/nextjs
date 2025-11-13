743eb962f9d45c97880945e6767c1d1549e19477
Alok Singh <44195690+aalokvsingh@users.noreply.github.com>
2025-11-13 04:49:23 +0000

    bug fixes

--- src/app/fetchapi/clientside/page.tsx
+++ src/app/fetchapi/clientside/page.tsx
@@ -1,30 +1,32 @@
 'use client'
+
 import { useSearchParams } from 'next/navigation';
 import { useEffect,useState } from 'react';
 
-const fetchData = () =>{
-    const [data, setData] = useState({});
+export default function FetchData(){
+    const [data1, setData] = useState({});
 
     const searchParams = useSearchParams();
     const name =  searchParams.get('name');
     if(!name) throw new Error('Name query parameter is required');
     
+    const APIcall = async () => {
+            const res = await fetch(`https://fakestoreapi.com/users/${name}`);
+            if(!res.ok) throw new Error('Failed to fetch data');
+            const data1 =  await res.json();
+            setData(data1);
+            // console.log(data);
+        }
+
     useEffect(() => {
         // This will run on the client side after the component mounts
         console.log('Component mounted');
-        const APIcall = async () => {
-            const res = await fetch(`https://fakestoreapi.com/users/${name}`);
-            if(!res.ok) throw new Error('Failed to fetch data');
-            const data =  await res.json();
-            setData(data);
-            // console.log(data);
-        }
         
         APIcall();
     }, []);
     
     return (
-        <pre><h1>API Data: {JSON.stringify(data, null, 2)}</h1></pre>
+        <pre><h1>API Data: {JSON.stringify(data1, null, 2)}</h1></pre>
     );
 };
-export default fetchData;
+
